aa30e3084e8eaea7c6af6ec4060f551629866d83
Animesh.MS <123883011+Animiiexe@users.noreply.github.com>
2025-01-02 18:34:43 +0530

    Delete src/dark.js

--- src/dark.js
+++
@@ -1,7 +0,0 @@
-function onClick() {
-    var element = document.body;
-    element.classList.toggle("dark-mode",);
-    var elements = querySelector('card');
-    elements.classList.toggle("darkmode-card");
-  }
-  
